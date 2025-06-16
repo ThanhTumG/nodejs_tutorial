2f6c2d2045209149cf9e8110f614cd8fae1b6b62
ThanhTumg <thanhtumg.2510@gmail.com>
2025-06-16 22:17:15 +0700

    Feature: Init Task Manager Project

---
+++ task-manager/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/task");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Task manager app");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
