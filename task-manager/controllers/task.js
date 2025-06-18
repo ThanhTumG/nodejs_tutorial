690ced6ead2c4d7e265c23b17dcbff14e6070697
ThanhTumg <thanhtumg.2510@gmail.com>
2025-06-18 10:05:16 +0700

    Feature: Custom Errors

--- task-manager/controllers/task.js
+++ task-manager/controllers/task.js
@@ -1,5 +1,6 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -17,12 +18,12 @@ const getTask = asyncWrapper(async (req, res) => {
 
   // If task === null
   if (!task) {
-    return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    return next(createCustomError(`No task with id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
     new: true,
@@ -31,7 +32,7 @@ const updateTask = asyncWrapper(async (req, res) => {
 
   // If task === null
   if (!task) {
-    return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    return next(createCustomError(`No task with id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -42,7 +43,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
 
   // If task === null
   if (!task) {
-    return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    return next(createCustomError(`No task with id: ${taskID}`, 404));
   }
   res.status(200).json({ status: "success" });
 });
