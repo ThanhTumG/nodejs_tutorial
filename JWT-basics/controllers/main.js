5900cb209b5e95783b8eaf7cae5cbc1dec46f5b3
ThanhTumg <thanhtumg.2510@gmail.com>
2025-06-21 10:43:57 +0700

    Feature[JWT basics project]: Implement JWT authentication and custom error handling

--- JWT-basics/controllers/main.js
+++ JWT-basics/controllers/main.js
@@ -1,18 +1,29 @@
-const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const { BadRequest } = require("../errors");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
-    throw new CustomAPIError("Please provide email and password", 400);
+    throw new BadRequest("Please provide email and password");
   }
 
-  res.send("Login");
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "User created", token });
 };
 
 const dashboard = async (req, res) => {
-  const token = Math.floor(Math.random() * 1000);
-  res.status(200).json({ msg: token });
+  const luckyNumber = Math.floor(Math.random() * 100);
+
+  res.status(200).json({
+    msg: `Hello ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
